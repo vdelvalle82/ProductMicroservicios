df579dc670c26607e3a906b6334ad0c50d7355c5
Virginia <vdelvalle82@hotmail.com>
2025-01-26 20:47:31 +0100

    Actualizo a la versión spring boot 3.0.2 y Java 17

--- src/main/java/com/wannacode/product_microservice/ProductMicroserviceApplication.java
+++ src/main/java/com/wannacode/product_microservice/ProductMicroserviceApplication.java
@@ -2,7 +2,8 @@ package com.wannacode.product_microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+
+
 
 @SpringBootApplication
 public class ProductMicroserviceApplication {
